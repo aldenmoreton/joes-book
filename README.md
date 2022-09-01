e592cdcefd410816087304f28e0238c8b57fdca9
Alden Moreton <aldenmoreton@Aldens-MacBook-Pro.local>
2022-08-31 21:39:30 -0500

    Connection to MongoDB is successful. New collections are created if they are not already available.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# joes-book
+# Joe's Book
+
+- [Next.js in Docker](https://youtu.be/aNh8iShFXto)
+	- [Source Code](https://github.com/vercel/next.js/tree/canary/examples/with-docker)
+- [Mongo.db in Docker](https://www.mongodb.com/compatibility/docker)
---
+++ lib/mongodb.ts
@@ -0,0 +1,54 @@
+import { MongoClient } from 'mongodb'
+
+if (!process.env.MONGODB_URI) {
+	throw new Error('Invalid environment variable: "MONGODB_URI"')
+}
+
+const uri = process.env.MONGODB_URI
+const options = {}
+
+let client: MongoClient
+let clientPromise: Promise<MongoClient>
+
+if (!process.env.MONGODB_URI) {
+	throw new Error('Please add your Mongo URI to .env.local')
+}
+
+if (process.env.NODE_ENV === 'development') {
+	// In development mode, use a global variable so that the value
+	// is preserved across module reloads caused by HMR (Hot Module Replacement).
+	if (!global._mongoClientPromise) {
+		client = new MongoClient(uri, options)
+		global._mongoClientPromise = client.connect()
+	}
+	clientPromise = global._mongoClientPromise
+} else {
+	// In production mode, it's best to not use a global variable.
+	client = new MongoClient(uri, options)
+	clientPromise = client.connect()
+}
+
+clientPromise.then(
+	(res) => {
+		console.log('listing collections')
+		const db = res.db("app")
+		db.listCollections({}, {nameOnly: true}).toArray().then(
+			(array) => {
+				let collectionNames = []
+				for (let i = 0; i < array.length; i++) {
+					collectionNames.push(array[i].name)
+				}
+				let desiredNames = ["books", "characters", "events", "picks"]
+				for (let name of desiredNames) {
+					if (!collectionNames.includes(name)) {
+						db.createCollection(name)
+					}
+				}
+			}
+		)
+	}
+)
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise
