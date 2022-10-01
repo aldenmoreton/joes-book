7935e103d03fb5b4f770ceae4f8991c9ab99fbcd
Alden Moreton <aldenmoreton99@gmail.com>
2022-10-01 10:51:08 -0500

    Remove errors for github build process

--- lib/mongodb.tsx
+++ lib/mongodb.tsx
@@ -1,8 +1,9 @@
 import { MongoClient } from 'mongodb'
 
-if (!process.env.MONGODB_URI) {
-	throw new Error('Invalid environment variable: "MONGODB_URI"')
-}
+//TODO: Is it safe to remove this check? Github requires it to build the image
+// if (!process.env.MONGODB_URI) {
+// 	throw new Error('Invalid environment variable: "MONGODB_URI"')
+// }
 
 const uri = process.env.MONGODB_URI
 const options = {}
