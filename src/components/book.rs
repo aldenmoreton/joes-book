1c266980533dffcf05af8f7360e5b374ceaa17a4
Alden Moreton <aldenmoreton99@gmail.com>
2023-08-13 22:04:37 -0500

    Create admin area, secure deletion

--- src/components/book.rs
+++ src/components/book.rs
@@ -1,11 +1,12 @@
 use leptos::*;
 use leptos_router::*;
 
-use crate::components::{books::Book, BookEvents};
+use crate::components::books::{Book, BookRole};
 
 #[server(GetBook, "/secure")]
 pub async fn get_book(cx: Scope, id: i64) -> Result<Book, ServerFnError> {
-	use crate::components::{auth, pool};
+	use crate::components::pool;
+	use crate::auth::auth;
 
 	let user = auth(cx)?.current_user.unwrap();
 	let pool = pool(cx)?;
@@ -28,11 +29,17 @@ pub async fn get_book(cx: Scope, id: i64) -> Result<Book, ServerFnError> {
 
 #[server(DeleteBook, "/secure")]
 pub async fn delete_book(cx: Scope, id: i64) -> Result<(), ServerFnError> {
-	use crate::components::{auth, pool};
-	use crate::components::books::BookRoles;
+	use crate::components::pool;
+	use crate::auth::auth;
+	use crate::components::books::BookRole;
 
 	let user = auth(cx)?.current_user.unwrap();
 	let pool = pool(cx)?;
+
+	match get_book(cx, id).await? {
+		Book { role: BookRole::Owner, .. } => (),
+		_ => return Err(ServerFnError::Request("You can't just delete someone elses book! Rude!!!".into()))
+	}
 
 	let deleted_book = sqlx::query!(
 		r#"	DELETE FROM books
@@ -44,7 +51,7 @@ pub async fn delete_book(cx: Scope, id: i64) -> Result<(), ServerFnError> {
 			RETURNING id"#,
 			user.id,
 			id,
-			Into::<String>::into(BookRoles::Owner)
+			Into::<String>::into(BookRole::Owner)
 	)
 		.fetch_one(&pool)
 		.await
@@ -76,23 +83,37 @@ pub fn Book(
 ) -> impl IntoView {
 	let params = use_params_map(cx);
 
-	let delete_book = create_server_action::<DeleteBook>(cx);
-
 	let book_id:i64 = params.with(|params| params.get("id").cloned()).unwrap().parse::<i64>().unwrap();
 
-	view!{cx,
-		// <Transition fallback=move || view! {cx, <p>"Loading..."</p> }>
-		// 	{move ||
-		// 		book =
-		// 	}
-		// </Transition>
-		// {book.read(cx).unwrap().unwrap().name}
-		// <p>{id}</p>
+	let book = create_resource(
+		cx,
+		|| (),
+		move |_| async move {
+			get_book(cx, book_id).await.unwrap()
+		}
+	);
 
+	view!{cx,
+		<Suspense fallback=|| "Loading">
+			{move || match book.read(cx) {
+				Some(Book{role: BookRole::Admin, ..}) |
+				Some(Book{role: BookRole::Owner, ..}) => AdminView(cx, AdminViewProps{id: book_id}).into_view(cx),
+				_ => ().into_view(cx)
+			}}
+		</Suspense>
+
+		<p>"Common knowlege"</p>
+	}
+}
+
+#[component]
+pub fn AdminView(cx: Scope, id: i64) -> impl IntoView {
+	let delete_book = create_server_action::<DeleteBook>(cx);
+
+	view! {cx,
 		<ActionForm action=delete_book>
-			<input type="hidden" name="id" value={book_id}/>
+			<input type="hidden" name="id" value={id}/>
 			<input type="submit" value="Delete Book"/>
 		</ActionForm>
 	}
-
 }
