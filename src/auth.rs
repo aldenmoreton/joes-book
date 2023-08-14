33756620efc551e9204bf5234b112283c000b84b
Alden Moreton <aldenmoreton99@gmail.com>
2023-08-13 20:44:34 -0500

    Refactor auth for frontend/backend

--- src/auth.rs
+++ src/auth.rs
@@ -1,25 +1,25 @@
 use cfg_if::cfg_if;
 use leptos::*;
-use serde::{Deserialize, Serialize};
-use std::collections::HashSet;
 
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
-pub struct User {
-    pub id: i64,
-    pub username: String,
-    pub password: String,
-    pub permissions: HashSet<String>,
-}
 
 cfg_if! {
     if #[cfg(feature = "ssr")] {
+        use std::collections::HashSet;
+
+        #[derive(Clone, Debug, PartialEq, Eq)]
+        pub struct BackendUser {
+            pub id: i64,
+            pub username: String,
+            pub password: String,
+            pub permissions: HashSet<String>,
+        }
+
         use async_trait::async_trait;
         use sqlx::PgPool;
         use axum_session_auth::{SessionPgPool, Authentication, HasPermission as HasPerm};
-        use crate::components::auth;
-        pub type AuthSession = axum_session_auth::AuthSession<User, i64, SessionPgPool, PgPool>;
+        pub type AuthSession = axum_session_auth::AuthSession<BackendUser, i64, SessionPgPool, PgPool>;
 
-        impl User {
+        impl BackendUser {
             pub async fn add_to_db(username: String, password: String, permissions: Vec<String>, pool: PgPool) -> anyhow::Result<Self> {
                 let password_hashed = bcrypt::hash(password.clone(), bcrypt::DEFAULT_COST)?;
 
@@ -43,14 +43,19 @@ cfg_if! {
                     .await?;
 
                 for permission in permissions.iter() {
-                    sqlx::query("INSERT INTO user_permissions (user_id, token) VALUES ($1, $2)")
+                    sqlx::query(
+                        r#" INSERT INTO user_permissions (user_id, token)
+                            SELECT $1, $2
+                            WHERE NOT EXISTS(
+                                SELECT 1 FROM user_permissions WHERE user_id=$1 AND token=$2
+                            )"#)
                         .bind(new_user.id)
                         .bind(permission)
                         .execute(&pool)
                         .await?;
                 }
 
-                Ok(User{
+                Ok(BackendUser{
                     id: new_user.id,
                     username,
                     password,
@@ -69,7 +74,7 @@ cfg_if! {
                     .await
                     .ok()?;
 
-                let mut user = User{
+                let mut user = BackendUser{
                     id: user_fields.id,
                     username: user_fields.username,
                     password: user_fields.password,
@@ -100,7 +105,7 @@ cfg_if! {
                     .await
                     .ok()?;
 
-                let mut user = User{
+                let mut user = BackendUser{
                     id: user_fields.id,
                     username: user_fields.username,
                     password: user_fields.password,
@@ -129,11 +134,11 @@ cfg_if! {
         }
 
         #[async_trait]
-        impl Authentication<User, i64, PgPool> for User {
-            async fn load_user(userid: i64, pool: Option<&PgPool>) -> Result<User, anyhow::Error> {
+        impl Authentication<BackendUser, i64, PgPool> for BackendUser {
+            async fn load_user(userid: i64, pool: Option<&PgPool>) -> Result<BackendUser, anyhow::Error> {
                 let pool = pool.unwrap();
 
-                User::get(userid, pool)
+                BackendUser::get(userid, pool)
                     .await
                     .ok_or_else(|| anyhow::anyhow!("Cannot get user"))
             }
@@ -152,28 +157,53 @@ cfg_if! {
         }
 
         #[async_trait]
-        impl HasPerm<PgPool> for User {
+        impl HasPerm<PgPool> for BackendUser {
             async fn has(&self, perm: &str, _pool: &Option<&PgPool>) -> bool {
                 self.permissions.contains(perm)
             }
         }
+        pub fn auth(cx: Scope) -> Result<AuthSession, ServerFnError> {
+			use_context::<AuthSession>(cx)
+				.ok_or("Auth session missing.")
+				.map_err(|e| ServerFnError::ServerError(e.to_string()))
+		}
     }
 }
 
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+#[cfg_attr(feature = "ssr", derive(sqlx::FromRow))]
+pub struct FrontendUser {
+    pub id: i64,
+    pub username: String
+}
+
 #[server(GetUser, "/secure")]
-pub async fn get_user(cx: Scope) -> Result<Option<User>, ServerFnError> {
+pub async fn get_user(cx: Scope) -> Result<FrontendUser, ServerFnError> {
+    let auth = auth(cx)?;
+    let BackendUser{ id, username, .. } = auth.current_user.unwrap();
+    Ok(FrontendUser{ id, username })
+}
+
+#[server(GetUsername, "/secure")]
+pub async fn get_username(cx: Scope) -> Result<String, ServerFnError> {
     let auth = auth(cx)?;
 
-    Ok(auth.current_user)
+    Ok(auth.current_user.unwrap().username)
+}
+
+#[server(GetUserID, "/secure")]
+pub async fn get_user_id(cx: Scope) -> Result<i64, ServerFnError> {
+    let auth = auth(cx)?;
+
+    Ok(auth.current_user.unwrap().id)
 }
 
 #[server(HasPermission, "/secure")]
 pub async fn has_permission(cx: Scope, permission: String) -> Result<bool, ServerFnError> {
-    match get_user(cx).await {
-        Ok(Some(user)) => {
+    match auth(cx)? {
+        AuthSession{current_user: Some(user), ..} => {
             Ok(user.permissions.contains(&permission))
-        }
-        Err(e) => Err(e),
+        },
         _ => Ok(false)
     }
 }
