5bcea6371ab9199584311dcf5511379802e54763
Alden Moreton <aldenmoreton99@gmail.com>
2023-08-19 23:36:20 -0500

    Add home page with some WIP

---
+++ src/components/home.rs
@@ -0,0 +1,20 @@
+use leptos::*;
+
+#[component]
+pub fn Home(
+    cx: Scope,
+) -> impl IntoView {
+
+    view! {
+        cx,
+        <form>
+			<select name="timezone" id="timezone">
+				<option value="Pacific/Kwajalein">Eniwetok, Kwajalein</option>
+				<option value="Pacific/Midway">Midway Island, Samoa</option>
+				<option value="Pacific/Honolulu">Hawaii</option>
+				<option value="Pacific/Marquesas">Taiohae</option>
+			</select>
+			<input type="datetime-local" id="meeting-time" name="meeting-time"/>
+		</form>
+    }
+}
