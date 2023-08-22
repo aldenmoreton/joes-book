e436ba7cd9efb58e16374f1a6f408016cf437ebd
Alden Moreton <aldenmoreton99@gmail.com>
2023-08-22 17:09:29 -0500

    Add tailwind support

--- src/components/home.rs
+++ src/components/home.rs
@@ -9,6 +9,7 @@ pub fn Home(
 
     view! {
         cx,
+		<h1 class="text-blue-500">"Green!"</h1>
         <form>
 			<select name="timezone" id="timezone">
 				<option value="Pacific/Kwajalein">Eniwetok, Kwajalein</option>
@@ -21,5 +22,15 @@ pub fn Home(
 		<form>
 			<TimezoneDropdown/>
 		</form>
+		<div class="inline-block fill-red-500  h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
+  			<span class="!absolute fill-red-500 !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+		</div>
+		<div
+  			class="inline-block h-8 w-8 animate-[spinner-grow_0.75s_linear_infinite] rounded-full bg-current align-[-0.125em] text-primary opacity-0 motion-reduce:animate-[spinner-grow_1.5s_linear_infinite]"
+  			role="status">
+  			<span
+    			class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+    			>Loading...</span>
+		</div>
     }
 }
