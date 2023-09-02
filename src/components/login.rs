19978e0a5ee379468b380e831eddd5374e7ed340
Alden Moreton <aldenmoreton99@gmail.com>
2023-09-02 10:13:49 -0500

    Upgrade login/signup pages

--- src/components/login.rs
+++ src/components/login.rs
@@ -11,25 +11,34 @@ pub fn Login(
 
     view! {
         cx,
-        <ActionForm action=login>
-            <h1>"Log In"</h1>
-            <label>
-                "User ID:"
-                <input type="text" placeholder="User ID" maxlength="32" name="username" class="auth-input" />
-            </label>
-            <br/>
-            <label>
-                "Password:"
-                <input type="password" placeholder="Password" name="password" class="auth-input" />
-            </label>
-            <br/>
-            <label>
-                <input type="checkbox" name="remember" class="auth-input" />
-                "Remember me?"
-            </label>
-            <br/>
-            <button type="submit" class="button">"Log In"</button>
-        </ActionForm>
-        <p>"Don't have an account? "<a href="signup">"Sign Up"</a></p>
+        <div class="flex flex-col items-center justify-center pt-10">
+            <div class="w-full max-w-xs">
+                <ActionForm action=login class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div class="mb-4">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="Username"/>
+                    </div>
+                    <div class="mb-6">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="Password"/>
+                    <label>
+                            "Remember me?"
+                            <input type="checkbox" name="remember" class="auth-input" />
+                        </label>
+                    </div>
+                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        Sign In
+                    </button>
+                    <div class="font-bold text-sm pt-3">
+                    <p>"Don't have an account?"</p>
+                    <a class="text-green-500 hover:text-green-800" href="/signup">"Create Account"</a>
+                    </div>
+                </ActionForm>
+            </div>
+        </div>
     }
 }
--- src/components/signup.rs
+++ src/components/signup.rs
@@ -12,31 +12,32 @@ pub fn Signup(
 
     view! {
         cx,
-        <ActionForm action=signup>
-            <h1>"Sign Up"</h1>
-            <label>
-                "User ID:"
-                <input type="text" placeholder="User ID" maxlength="32" name="username" class="auth-input" />
-            </label>
-            <br/>
-            <label>
-                "Password:"
-                <input type="password" placeholder="Password" name="password" class="auth-input" />
-            </label>
-            <br/>
-            <label>
-                "Confirm Password:"
-                <input type="password" placeholder="Password again" name="password_confirmation" class="auth-input" />
-            </label>
-            <br/>
-            <label>
-                "Remember me?"
-                <input type="checkbox" name="remember" class="auth-input" />
-            </label>
-
-            <br/>
-            <button type="submit" class="button">"Sign Up"</button>
-        </ActionForm>
-        <p>"Have an account? "<a href="login">"Login"</a></p>
+        <div class="flex flex-col items-center justify-center pt-10">
+            <div class="w-full max-w-xs">
+                <ActionForm action=signup class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                    <div class="mb-4">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">"Username"</label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="Username"/>
+                    </div>
+                    <div class="mb-6">
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">"Password"</label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="Password"/>
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password_confirmation">"Confirm Password"</label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password_confirmation" name="password_confirmation" type="password" placeholder="Password"/>
+                        <label>
+                            "Remember me?"
+                            <input type="checkbox" name="remember" class="auth-input" />
+                        </label>
+                    </div>
+                    <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                        "Sign Up"
+                    </button>
+                    <div class="font-bold text-sm pt-3">
+                        <p>"Already have an account?"</p>
+                        <a class="text-green-500 hover:text-green-800" href="/login">"Sign In"</a>
+                    </div>
+                </ActionForm>
+            </div>
+        </div>
     }
 }
