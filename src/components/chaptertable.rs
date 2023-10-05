2dfbfb168d50f1d30898baa6f54f9fc52684547b
Alden Moreton <aldenmoreton99@gmail.com>
2023-10-05 10:54:25 -0500

    Add ability to grade chapter for owner

--- src/components/chaptertable.rs
+++ src/components/chaptertable.rs
@@ -1,11 +1,12 @@
 use leptos::*;
 use leptos_router::use_params_map;
 
-use crate::server::get_chapter_table;
+use crate::{server::{get_chapter_table, get_book}, objects::{BookSubscription, BookRole}};
 
 #[component]
 pub fn ChapterTable(cx: Scope) -> impl IntoView {
 	let params = use_params_map(cx);
+	let book_id: i64 = params.with_untracked(|params| params.get("book_id").cloned()).unwrap().parse::<i64>().unwrap();
 	let chapter_id: i64 = params.with_untracked(|params| params.get("chapter_id").cloned()).unwrap().parse::<i64>().unwrap();
 
 	let table_fetcher = create_resource(cx, || (),
@@ -13,9 +14,15 @@ pub fn ChapterTable(cx: Scope) -> impl IntoView {
 	);
 
 	view!{cx,
-		"Chapter table!"
+		<br/>
+		<Await future=move |_| get_book(cx, book_id) bind:subscription>
+			{match subscription {
+				Ok(BookSubscription{role: BookRole::Owner, ..}) => view!{cx, <a href=format!("/books/{book_id}/chapters/{chapter_id}/grade") class="bg-green-200 border border-black rounded-md">"Grade Picks"</a>}.into_view(cx),
+				_ => ().into_view(cx)
+			}}
+		</Await>
 		<Transition fallback=move || "Loaading".into_view(cx)>
-			{
+			{move ||
 				table_fetcher
 					.read(cx)
 					.map(|table|
