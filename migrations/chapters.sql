0f8eef8a8bf329a255d7d3872bb10711a2bc485c
Alden Moreton <aldenmoreton99@gmail.com>
2023-10-17 16:23:07 -0500

    Continue to remove timestamps from required schema

--- migrations/chapters.sql
+++ migrations/chapters.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS chapters (
 	title       	TEXT NOT NULL,
 	book_id			INT8 NOT NULL REFERENCES books(id),
 	is_open			BOOLEAN NOT NULL,
-	closing_time	TIMESTAMPTZ NOT NULL,
+	closing_time	TIMESTAMPTZ DEFAULT NULL,
 	created_at 		TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	notes 			TEXT
 );
