a3bbf3a71994fba78da08daac89dc05d88c64d97
Alden Moreton <aldenmoreton99@gmail.com>
2024-04-15 19:40:11 -0500

    Start book creation endpoint

--- src/routes/book/endpoints.rs
+++ src/routes/book/endpoints.rs
@@ -1,3 +1,16 @@
-use axum::response::IntoResponse;
+use axum::{
+    response::{Html, IntoResponse},
+    Form,
+};
+use serde::Deserialize;
 
-pub async fn create() -> impl IntoResponse {}
+#[derive(Deserialize)]
+pub struct CreateParams {
+    #[serde(rename(deserialize = "book-name"))]
+    book_name: String,
+}
+
+pub async fn create(Form(CreateParams { book_name }): Form<CreateParams>) -> impl IntoResponse {
+    println!("{book_name}");
+    Html("<p>About to make a book</p>")
+}
