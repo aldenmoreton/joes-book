e73ae2705cd77c574d2e39cccbda4456f9c98f6e
Alden Moreton <aldenmoreton99@gmail.com>
2024-08-20 16:37:45 -0500

    Shuttle

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use axum_login::{
     AuthManagerLayerBuilder,
 };
 use joes_book::{auth::BackendPgDB, router};
-use sqlx::{postgres::PgPoolOptions, PgPool};
+use sqlx::PgPool;
 
 use tower_sessions::PostgresStore;
 
@@ -12,16 +12,8 @@ pub struct AppState {
     pub db: PgPool,
 }
 
-#[tokio::main]
-async fn main() {
-    dotenvy::dotenv().ok();
-    let database_url = std::env::var("DATABASE_URL").expect("Unable to read DATABASE_URL ENV");
-
-    let pool = PgPoolOptions::new()
-        .connect(&database_url)
-        .await
-        .expect("Could not make pool.");
-
+#[shuttle_runtime::main]
+pub async fn shuttle(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_axum::ShuttleAxum {
     let backend = BackendPgDB(pool.clone());
     backend.init_admin().await.ok();
 
@@ -35,10 +27,37 @@ async fn main() {
         .with_expiry(Expiry::OnInactivity(Duration::weeks(2)));
     let auth_layer = AuthManagerLayerBuilder::new(backend, session_layer).build();
 
-    let app = router(auth_layer);
-
-    println!();
-    println!("Starting server at http://localhost:3000");
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    Ok(shuttle_axum::AxumService(router(auth_layer)))
 }
+
+// #[tokio::main]
+// async fn main() {
+// use postgres::PgPoolOptions
+//     dotenvy::dotenv().ok();
+//     let database_url = std::env::var("DATABASE_URL").expect("Unable to read DATABASE_URL ENV");
+
+//     let pool = PgPoolOptions::new()
+//         .connect(&database_url)
+//         .await
+//         .expect("Could not make pool.");
+
+//     let backend = BackendPgDB(pool.clone());
+//     backend.init_admin().await.ok();
+
+//     let session_store = PostgresStore::new(pool);
+//     session_store
+//         .migrate()
+//         .await
+//         .expect("Could not migrate database");
+//     let session_layer = SessionManagerLayer::new(session_store)
+//         .with_secure(false)
+//         .with_expiry(Expiry::OnInactivity(Duration::weeks(2)));
+//     let auth_layer = AuthManagerLayerBuilder::new(backend, session_layer).build();
+
+//     let app = router(auth_layer);
+
+//     println!();
+//     println!("Starting server at http://localhost:3000");
+//     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+//     axum::serve(listener, app).await.unwrap();
+// }
