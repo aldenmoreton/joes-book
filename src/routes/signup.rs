c668838ea0a3c9a2760bc6a49cd3e9ac6496b633
Alden Moreton <aldenmoreton99@gmail.com>
2024-08-26 11:06:49 -0500

    Add turnstile to sign up page

--- src/routes/signup.rs
+++ src/routes/signup.rs
@@ -1,12 +1,16 @@
 use axum::{
+    extract::State,
     response::{IntoResponse, Redirect},
     Form,
 };
 use axum_ctx::{RespErrCtx, RespErrExt, StatusCode};
 
-use crate::{auth::AuthSession, templates::base, AppError, AppNotification};
+use crate::{auth::AuthSession, templates::base, AppError, AppNotification, AppStateRef};
 
-pub async fn signup_page(auth_session: AuthSession) -> impl IntoResponse {
+pub async fn signup_page(
+    auth_session: AuthSession,
+    State(state): State<AppStateRef>,
+) -> impl IntoResponse {
     if auth_session.user.is_some() {
         return Redirect::to("/").into_response();
     }
@@ -15,33 +19,49 @@ pub async fn signup_page(auth_session: AuthSession) -> impl IntoResponse {
 		Some("Sign Up"),
 		Some(maud::html!(
             (crate::templates::alertify())
+            script src="https://challenges.cloudflare.com/turnstile/v0/api.js?onload=onloadTurnstileCallback" defer {}
+            script {
+                "window.onloadTurnstileCallback = function () {
+                    turnstile.render('#cf-turnstile-container', {
+                        sitekey: '"(state.turnstile.site_key)"',
+                        callback: function(token) {
+                            document.getElementById('submit-button').disabled = false;
+                        },
+                        theme: 'light',
+                        action: 'signup',
+                    });
+                };"
+            }
         )),
 		None,
 		Some(maud::html!(
 		div class="flex flex-col items-center justify-center pt-10" {
 			div class="w-full max-w-xs" {
-				form hx-post="/signup" class="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md" {
-					div class="mb-4" {
-						label class="block mb-2 text-sm font-bold text-gray-700" for="username" {
-							"Username"
-						}
-						input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="Username";
-					}
-					div class="mb-6" {
-						label class="block mb-2 text-sm font-bold text-gray-700" for="password" { "Password" }
-						input class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="Password";
+				form hx-post="/signup" hx-on--after-on-load="if (event.detail.xhr.status !== 200) {document.getElementById('submit-button').disabled = true;turnstile.reset('#cf-turnstile-container');}" {
+                    div class="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md" {
+                        div class="mb-4" {
+                            label class="block mb-2 text-sm font-bold text-gray-700" for="username" {
+                                "Username"
+                            }
+                            input class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="username" name="username" type="text" placeholder="Username";
+                        }
+                        div class="mb-6" {
+                            label class="block mb-2 text-sm font-bold text-gray-700" for="password" { "Password" }
+                            input class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="Password";
 
-						label class="block mb-2 text-sm font-bold text-gray-700" for="password_confirmation" { "Confirm Password" }
-						input id="password_confirmation" name="password_confirmation" type="password" placeholder="Password" class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline";
-					}
-					button class="px-4 py-2 font-bold text-white bg-green-500 rounded hover:bg-green-700 focus:outline-none focus:shadow-outline" type="submit" {
-						"Sign Up"
-					}
-					div class="pt-3 text-sm font-bold" {
-						p { "Already have an account?" }
-						a class="text-green-500 hover:text-green-800" href="/login" { "Sign In" }
-					}
+                            label class="block mb-2 text-sm font-bold text-gray-700" for="password_confirmation" { "Confirm Password" }
+                            input id="password_confirmation" name="password_confirmation" type="password" placeholder="Password" class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline";
+                        }
+                        button id="submit-button" class="px-4 py-2 font-bold text-white bg-green-500 rounded disabled:cursor-wait disabled:bg-gray-400 hover:bg-green-700 focus:outline-none focus:shadow-outline" type="submit" {
+                            "Sign Up"
+                        }
+                    }
+                    div id="cf-turnstile-container" {}
 				}
+                div class="pt-3 text-sm font-bold" {
+                    p { "Already have an account?" }
+                    a class="text-green-500 hover:text-green-800" href="/login" { "Sign In" }
+                }
 			}
 		}
     )),
@@ -53,11 +73,33 @@ pub struct SignUpForm {
     username: String,
     password: String,
     password_confirmation: String,
+    #[serde(rename = "cf-turnstile-response")]
+    pub turnstile_response: String,
 }
 pub async fn signup_form(
     mut auth_session: AuthSession,
-    form: Form<SignUpForm>,
+    State(state): State<AppStateRef>,
+    Form(form): Form<SignUpForm>,
 ) -> Result<impl IntoResponse, AppNotification> {
+    let cf_validate: Result<cf_turnstile::SiteVerifyResponse, cf_turnstile::error::TurnstileError> =
+        state
+            .turnstile
+            .client
+            .siteverify(cf_turnstile::SiteVerifyRequest {
+                response: form.turnstile_response,
+                ..Default::default()
+            })
+            .await;
+
+    tracing::debug!("{cf_validate:?}");
+
+    if !cf_validate.map(|v| v.success).unwrap_or(false) {
+        return Err(AppNotification(
+            StatusCode::UNAUTHORIZED,
+            "You did not pass our check for robots".into(),
+        ));
+    }
+
     let pool = auth_session.backend.0.clone();
 
     if form.password != form.password_confirmation {
